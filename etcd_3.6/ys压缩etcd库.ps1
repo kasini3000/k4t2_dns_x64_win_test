1810fb8a4e878648d3a11755034e9ef46891e7b4
chuanjiao10 <1328486072@qq.com>
2026-03-30 14:17:24 +0800

    修复：etcd脚本

--- etcd_3.6/ys压缩etcd库.ps1
+++ etcd_3.6/ys压缩etcd库.ps1
@@ -1,7 +1,16 @@
 ﻿#
 Set-Location ${PSScriptRoot}
 
-$rev = (etcdctl endpoint status --write-out="json" | ConvertFrom-Json)[0].header.revision
-etcdctl compact $rev
+if ( ($IsWindows -eq $True) -or ($PSVersionTable.psversion.major -lt 6) ) #win
+{
+  $rev = (./etcdctl endpoint status --write-out="json" | ConvertFrom-Json)[0].Status.header.revision
+  ./etcdctl compact $rev
+}
+else 
+{
+  $rev = (etcdctl endpoint status --write-out="json" | ConvertFrom-Json)[0].header.revision
+  etcdctl compact $rev
+}   
+
   
   
